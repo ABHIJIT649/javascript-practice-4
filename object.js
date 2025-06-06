d901b5680c351e5e3a880ea1f056efab14046807
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-06-06 10:18:51 +0530

    add some object releted problem

---
+++ object.js
@@ -0,0 +1,16 @@
+const myId ={
+    name : "Ravi",
+    age : 24
+}
+console.log(myId);
+//acess a single value name
+console.log(myId.name);
+console.log(myId["name"]);
+
+//add a new preority in oibject
+myId.city = "Delhi" 
+console.log(myId)
+
+//change the value of on exiting preority
+
+
