71fef1c1cad75560efa609a7abb7930e0bc59351
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-06-25 21:40:20 +0530

    add some js dome practice question an solution

---
+++ DOM/dom_01.js
@@ -0,0 +1,31 @@
+//Given an <h1> with id title, change its text to "Hello World!" using JS
+
+document.querySelector('h1');
+
+const change = document.querySelector('h1')
+change.innerText = "Helllo World"
+
+
+//Given a <p> with id message, change its text color to red
+
+document.querySelector('#message');
+
+const changeColor = document.querySelector('#message');
+
+changeColor.style.color = "red"
+
+
+//Given a <ul> with id myList, add a new <li> with text "New Item"
+
+document.querySelectorAll('li')
+
+const changeList = document.querySelectorAll('#myList');
+
+changeList[1].innerText = "new item"
+
+
+
+
+
+
+
